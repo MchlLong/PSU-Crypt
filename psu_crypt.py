0d02a79d2c13746d5973bad23fd2456f1a3e3552
Michael Long <50342663+MchlLong@users.noreply.github.com>
2020-02-17 11:35:48 -0800

    Cleanup
    Added comments, renamed some variables for clarity.

--- psu_crypt.py
+++ psu_crypt.py
@@ -22,15 +22,17 @@ def _feistel():
 def _keystream(key, key_length, direction):
     # Generate keystream from key
     # Returns list of keys
+
     # Verify that key is 64 bits, 80 bits, or 128 bits
     # Verify that the key's length divides evenly into 
     if (key_length % 8 != 0):
         return -1
+
     ret = []
     temp_key = key          # Standard shift key used to generate 0 ~ 191 K' values
     temp_key_truncate = 0   # Temporary shift key to do many right shifts to truncate and obtain the proper value
-    counter_truncate = 0
-    counter_side_swap = 0
+    counter_truncate = 0    # Counter to truncate 'X' bytes where 'X' is two times the counter
+    counter_side_swap = 0   # Counter to swap between right half of key and left half of key
 
     for i in range(192):
 
@@ -54,31 +56,33 @@ def _keystream(key, key_length, direction):
                     temp_key_truncate = (_bit_rotate(temp_key_truncate, 64, RIGHT))
         
 
-        # Update stuff
-        add = temp_key_truncate & 0xFF
-        ret.append(int.to_bytes(add, 1, "big").hex())
-        counter_truncate = (counter_truncate + 1) % 4
-        counter_side_swap = (counter_side_swap + 1) % 24
+        # Update loop control variables and ret value
+        add = temp_key_truncate & 0xFF                   # Truncate excess bits to keep the subkeys 32 bits
+        ret.append(int.to_bytes(add, 1, "big").hex())    # Add to return list
+        counter_truncate = (counter_truncate + 1) % 4    # Increase the amount of shifts needed to reach next key block
+        counter_side_swap = (counter_side_swap + 1) % 24 # Increase the amount of checks before swapping halves
 
-    # Work before return
-    print(ret)
-    print(f'Key After Stream: {int.to_bytes(temp_key, 8, "big").hex()}')
+    # DEBUG USE ONLY
+    """
+    print(ret)  # Validate keystream
+    print(f'Key After Stream: {int.to_bytes(temp_key, 8, "big").hex()}') # Validate it rotated completely through
+    """
+    # Return list of key blocks
     return ret
 
 def _bit_rotate(key, key_len, dir):
 
-    # For "Left Rotation"
-    # Placehold overflow bits
+    # Rotate left
     if (dir == LEFT):
-        # Leftmost bit truncated
-        left_truncate = (2 ** key_len)-2
-        reg = ((key << 1) & left_truncate)
-        overflow = key >> (key_len - 1)
+        left_truncate = (2 ** key_len)-2    # Equivelant to 0xFFFF FFFF FFFF FFFE to drop the right most bit
+        reg = ((key << 1) & left_truncate)  # Shift left, then drop the rightmost bit
+        overflow = key >> (key_len - 1)     # Shift to catch the leftmost bit to put on the righthand bit slot
 
+    # Rotate right
     if (dir == RIGHT):
-        right_truncate = 2 ** (key_len - 1)
-        reg = key >> 1
-        overflow = (key << (key_len - 1)) & right_truncate
+        right_truncate = 2 ** (key_len - 1)                 # Equivelant to 0xEFFF FFFF FFFF FFFF FFFF to drop the leftmost bit
+        reg = key >> 1                                      # Shift right, then drop the leftmost bit
+        overflow = (key << (key_len - 1)) & right_truncate  # Shift to catch the rightmost bit to put on the lefthand bit slot
     
-    # OR the two (Normal Bytes and a bunch of 0's with a 1 or 0)
+    # Bitwise OR to append the overflow bit to the dropped bit position
     return (reg|overflow)
