27e124451002cb5de6baf6448a12528a15332c41
Michael Long <50342663+MchlLong@users.noreply.github.com>
2020-02-17 20:34:14 -0800

    Modifications made to generate sub words (w0 ~ w3)
    ---

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ import psu_crypt
 
 def main():
     test_key = 0xabcdef0123456789
+    test_plain = 0x0123456789abcdef
     key_len = 64
-    print(f'Key Before Stream: {int.to_bytes(test_key, 8, "big").hex()}')
+    #print(f'Key Before Stream: {int.to_bytes(test_key, 8, "big").hex()}')
     ret = psu_crypt._keystream(test_key, key_len, 0)
+    print(psu_crypt._encrypt_block(test_plain, test_key))
+    #print(ret)
+    print('-  - - - - -  -')
+    print(psu_crypt.f_table)
     pass
 
 if __name__ == '__main__':
