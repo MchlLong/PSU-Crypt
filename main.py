73697a37430cb8cdf9be9bc2b139875dd83dff24
Michael Long <50342663+MchlLong@users.noreply.github.com>
2020-02-18 23:54:34 -0800

    Cleaned up and comments
    Need additional file IO and a simple padding scheme to test on multiple words

--- main.py
+++ main.py
@@ -6,14 +6,14 @@
 import psu_crypt
 
 def main():
+    
     test_key = 0xabcdef0123456789
     test_plain = 0x0123456789abcdef
-    key_len = 64
 
     ret = psu_crypt._encrypt_block(test_plain, test_key)
     print(f'Cipher Text: {int.to_bytes(ret, 8, "big").hex()}')
     ret2 = psu_crypt._decrypt_block(ret, test_key)
-    print(f'Cipher Text: {int.to_bytes(ret2, 8, "big").hex()}')
+    print(f'Plaintext Text: {int.to_bytes(ret2, 8, "big").hex()}')
 
     pass
 
